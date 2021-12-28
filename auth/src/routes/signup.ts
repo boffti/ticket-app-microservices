f8a7284dc286064efecfe3c29d6e26f2eded837c
Aneesh Melkot <boffti@gmail.com>
2021-12-28 11:15:00 -0600

    Added Auth Routes

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+const router = express.Router();
+
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email address'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Please enter a valid password')
+], (req: Request, res: Response) => {
+    const { email, password } = req.body;
+});
+
+export { router as signupRouter };
