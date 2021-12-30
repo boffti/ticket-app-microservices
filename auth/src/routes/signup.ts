e6a5018afbbb51ab7668fe937abfb8d70ce3e1d1
Aneesh Melkot <boffti@gmail.com>
2021-12-29 22:32:25 -0600

    Added error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -12,7 +14,16 @@ router.post('/api/users/signup', [
         .isLength({ min: 4, max: 20 })
         .withMessage('Please enter a valid password')
 ], (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
     const { email, password } = req.body;
+
+    console.log('Creating a user...');
+    throw new DatabaseConnectionError();
+
+    res.send({});
 });
 
 export { router as signupRouter };
